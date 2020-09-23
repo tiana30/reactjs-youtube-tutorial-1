6a20e4944e41d3b93ae355b6944f30f56bb29529
tiana30 <tiana3059@gmail.com>
2020-09-23 11:38:08 +0700

    15-post-data-with-json-server-api

--- src/containers/BlogPost/BlogPost.jsx
+++ src/containers/BlogPost/BlogPost.jsx
@@ -6,7 +6,7 @@ import Axios from 'axios';
 class BlogPost extends Component {
     state = {
         post: [],
-        formPost: {
+        formBlogPost: {
             userId: 1,
             id: 1,
             title: '',
@@ -15,12 +15,27 @@ class BlogPost extends Component {
     }
 
     getPostApi = () => {
-        Axios.get(`http://localhost:3004/posts/`)
+        Axios.get(`http://localhost:3004/posts?_sort=id&_order=desc`)
         .then((result) => {
             this.setState({
                 post: result.data
             })
         })
+    }
+
+    postDatatoApi = () => {
+        Axios.post(`http://localhost:3004/posts/`, this.state.formBlogPost)
+        .then((res) => 
+        {
+            console.log(res);
+            this.getPostApi();
+            
+        }, (err) => {
+            console.log("error :", err)
+        }
+        
+        )
+       
     }
 
     handleRemove = (data)  => {
@@ -34,16 +49,21 @@ class BlogPost extends Component {
 
     handleFormChange = (event) => {
         console.log('form change', event.target.value);
-        let formPostNew = {...this.state.formPost};
-        formPostNew = [event.target.name] = event.target.value;
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        formBlogPostNew['id'] = timestamp;
+        formBlogPostNew[event.target.name] = event.target.value;
 
         this.setState({
-            formBlogPost: formPostNew
+            formBlogPost: formBlogPostNew
         }, () => {
                 console.log('value object : ', this.state.formBlogPost)
         })
     }
 
+    handleSubmit = () => {
+       this.postDatatoApi();
+    }
     componentDidMount() {
         // fetch('https://jsonplaceholder.typicode.com/posts')
         // .then(response => response.json())
@@ -64,7 +84,7 @@ class BlogPost extends Component {
                     <input type="text" name="title" placeholder="add title" onChange={this.handleFormChange} />
                     <label htmlFor="body">Blog Content</label>
                     <textarea name="body" id="body" cols="30" rows="10" onChange={this.handleFormChange}></textarea>
-                    <button className="btn-submit">Simpan</button>
+                    <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
                 </div>
                 {
                     this.state.post.map(post => {
